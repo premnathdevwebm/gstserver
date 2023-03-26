e67243f96b541504c7b44aec9062faaef3f9c70c
premnath pichaimuthu <premnathlearny@gmail.com>
2023-03-26 08:11:00 +0530

    initial 3

--- routes/health.js
+++ routes/health.js
@@ -1,11 +1,11 @@
 const Router = require("express").Router();
 
+Router.get("/test", (req, res) => {
+  return res.json({ message: "Working test" });
+});
 Router.get("/", (req, res) => {
   return res.json({ message: "Working" });
 });
 
-Router.get("test/", (req, res) => {
-  return res.json({ message: "Working test" });
-});
 
 module.exports = Router;
