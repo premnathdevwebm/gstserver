fbd985b296992b5758ec8eed5f856dacb8b6f44c
premnath pichaimuthu <premnathlearny@gmail.com>
2023-03-27 01:59:21 +0530

    initial 10

--- models/users.js
+++ models/users.js
@@ -12,6 +12,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  role: {
+    type: String,
+    default: 'basic',
+    enum: ["basic", "admin"]
+   },
   hash: String,
   salt: String
 });
