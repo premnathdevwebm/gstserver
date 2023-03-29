d6fc0d9ec73868614eb3ce430c44951a0796b489
premnath pichaimuthu <premnathlearny@gmail.com>
2023-03-29 20:08:41 +0530

    initial 15

--- models/commision.js
+++ models/commision.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 
 const rangeSchema = new mongoose.Schema({
+  title: {
+    name: String,
+  },
+  data: [{
   label: String,
   value: {
     type: mongoose.Types.ObjectId,
     default: () => new mongoose.Types.ObjectId()
   },
   location: String,
+}]
 });
 
 const divisionSchema = new mongoose.Schema({
