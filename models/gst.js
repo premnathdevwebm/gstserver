f446ea57e3b2249f12cdc1c5862c645cb8f8e0c0
premnath pichaimuthu <premnathlearny@gmail.com>
2023-04-22 07:35:42 +0530

    gst modified

--- models/gst.js
+++ models/gst.js
@@ -3,7 +3,12 @@ const gstSchema = new mongoose.Schema({
     location: String,
     value: String,
     user: String,
-    gst: mongoose.Schema.Types.Mixed
+    appealorder: [mongoose.Schema.Types.Mixed],
+    oio: [mongoose.Schema.Types.Mixed],
+    predeposit: [mongoose.Schema.Types.Mixed],
+    recovery: [mongoose.Schema.Types.Mixed],
+    appeal: [mongoose.Schema.Types.Mixed],
+    scn: [mongoose.Schema.Types.Mixed],
 }, {timestamps: true});
 
 mongoose.model('Gst', gstSchema);
