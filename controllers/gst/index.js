4175c61eff8360ffb12cfdf632dcc099f40c8664
premnath pichaimuthu <premwebdev+dev@gmail.com>
2023-04-29 09:22:15 +0530

    new

--- controllers/gst/index.js
+++ controllers/gst/index.js
@@ -11,12 +11,12 @@ const postGst = async (req, res) => {
     const gst = await Gst.create({
       ...req.body,
       user: req.auth.name,
-      scn: { $push: req.body.scn },
-      oio: { $push: req.body.oio },
-      appealorder: { $push: req.body.appealorder },
-      predeposit: { $push: req.body.predeposit },
-      recovery: { $push: req.body.recovery },
-      appeal: { $push: req.body.appeal },
+      scn: req.body.scn,
+      oio: req.body.oio,
+      appealorder: req.body.appealorder,
+      predeposit: req.body.predeposit,
+      recovery: req.body.recovery,
+      appeal: req.body.appeal,
     });
     res.json(gst);
   } catch (err) {
@@ -31,6 +31,20 @@ const getGsts = async (req, res) => {
       });
     }
     const gsts = await Gst.find();
+    [...gsts].forEach((doc) => {
+      const temp = Object.entries(doc);
+      const tempMap = temp[2][1];
+      const {
+        appealorder,
+        oio,
+        predeposit,
+        recovery,
+        appeal,
+        scn,
+        ...restVal
+      } = tempMap;
+      console.log(appealorder, oio, predeposit, recovery, appeal, scn, restVal);
+    });
     res.json(gsts);
   } catch (err) {
     return res.status(404);
@@ -50,20 +64,21 @@ const getGst = async (req, res) => {
   }
 };
 const updateGst = async (req, res) => {
- 
   try {
     if (!req.auth._id) {
       return res.status(401).json({
         message: "UnauthorizedError: private gst",
       });
     }
-    const updateQuery = {}
-    updateQuery[`scn.${req.params.index}`] = { ...req.body.scn }
-    updateQuery[`oio.${req.params.index}`] = { ...req.body.oio }
-    updateQuery[`appealorder.${req.params.index}`] = { ...req.body.appealorder }
-    updateQuery[`predeposit.${req.params.index}`] = { ...req.body.predeposit }
-    updateQuery[`recovery.${req.params.index}`] = { ...req.body.recovery }
-    updateQuery[`appeal.${req.params.index}`] = { ...req.body.appeal }
+    const updateQuery = {};
+    updateQuery[`scn.${req.params.index}`] = { ...req.body.scn };
+    updateQuery[`oio.${req.params.index}`] = { ...req.body.oio };
+    updateQuery[`appealorder.${req.params.index}`] = {
+      ...req.body.appealorder,
+    };
+    updateQuery[`predeposit.${req.params.index}`] = { ...req.body.predeposit };
+    updateQuery[`recovery.${req.params.index}`] = { ...req.body.recovery };
+    updateQuery[`appeal.${req.params.index}`] = { ...req.body.appeal };
     const gst = await Gst.findOneAndUpdate(
       { _id: req.params.id },
       {
